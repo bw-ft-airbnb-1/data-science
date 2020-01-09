33c648ecd1b276e0094cd911cd8a7db56d47fe85
bmulas1535 <bmulas1535@users.noreply.github.com>
2020-01-09 10:39:38 -0800

    update routes.py

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,11 +1,11 @@
 from webapp import app
-from flask import render_template
-
+from flask import render_template, url_for
+from flask_jsonify import jsonify
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
-@app.route('/addthis', methods=['GET', 'POST'])
-def addthis():
-	return f"{10 + 5}"
+@app.route('/price', methods=['GET', 'POST'])
+def price():
+    return 'Yay'
